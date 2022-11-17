26be33cd95e08bd702e7df0aa5f3b5a769aba048
Pavani131 <115334212+Pavani131@users.noreply.github.com>
2022-11-17 12:18:39 +0000

    Add files via upload

---
+++ Assignment1 c#/Assignment1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class calculator
+{
+    public static void Main()
+    {
+        int n1;
+        int n2;
+        int c;
+        int a = 0;
+        Console.WriteLine("Enter 1st number:");
+        n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter 2nd number:");
+        n2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Required task number:\n1-add\n2-subtract\n3-multiplication\n4-division");
+        c = int.Parse(Console.ReadLine());
+        switch (c)
+        {
+            case 1:
+                a = n1 + n2;
+                break;
+            case 2:
+                a = n1 - n2;
+                break;
+            case 3:
+                a = n1 * n2;
+                break;
+            case 4:
+                a = n1 / n2;
+                break;
+            default:
+                Console.WriteLine("Enter Correct Task number");
+                break;
+        }
+        Console.WriteLine("Answer is {0}", a);
+        Console.ReadLine();
+    }
+}
---
+++ Assignment1 c#/Assignment1/csassign1Q2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csassign2Q1
+{
+    internal class csassign1Q2
+    {
+       
+        
+            public static void main()
+            {
+                try
+                {
+                    int[] marks = new int[5];
+                    int sum = 0;
+                    float average;
+                    int highest = 0;
+                    Console.WriteLine("Enetr Marks:");
+                    for (int i = 0; i < marks.Length; i++)
+                    {
+                        marks[i] = int.Parse(Console.ReadLine());
+                    }
+                    for (int i = 0; i < marks.Length; i++)
+                    {
+                        sum += marks[i];
+                        if (marks[i] > highest)
+                        {
+                            highest = marks[i];
+                        }
+                    }
+                    average = sum / 5;
+                    Console.WriteLine("Average = (0) Highest(1)", average, highest);
+
+                }
+                catch (Exception Error)
+                {
+                    Console.WriteLine(Error.ToString());
+                }
+                finally
+                {
+                    Console.WriteLine("Final");
+                }
+                Console.ReadLine();
+
+            }
+
+        
+    }
+}
+
