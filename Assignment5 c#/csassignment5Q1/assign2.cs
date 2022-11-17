dcb48cc5becb9edf5a4efafae525c0f731a45bda
Pavani131 <115334212+Pavani131@users.noreply.github.com>
2022-11-17 12:24:13 +0000

    Add files via upload

---
+++ Assignment5 c#/csassignment5Q1/assign2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csassignment5Q1
+{
+    internal class assign2
+    {
+        public static void Main()
+        {
+
+            ArrayList Employee = new ArrayList();
+            Employee.Add(101);
+            Employee.Add("Pavani");
+            Employee.Add("Trainee");
+            Employee.Add(20000);
+            Employee.Add("Banglore");
+
+
+
+            foreach (object i in Employee)
+            {
+                Console.WriteLine(i + " ");
+
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+    
+
